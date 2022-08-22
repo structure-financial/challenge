02ee50bdc5dd409ef1c0bdfac64fe21505aa504a
Luke <47612057+skve@users.noreply.github.com>
2022-08-22 12:57:46 -0700

    Update InstrumentSelect.tsx

--- src/components/InstrumentSelect.tsx
+++ src/components/InstrumentSelect.tsx
@@ -4,26 +4,23 @@
 
 import Stock from "./Stock";
 import Coin from "./Coin";
+
 import { Instrument } from "@/types/api.d";
 
-interface Props {
+interface InstrumentSelectProps {
   coins: Instrument[];
   stocks: Instrument[];
 }
 
-const InstrumentSelect = ({ coins, stocks }: Props) => {
+const InstrumentSelect = ({ coins, stocks }: InstrumentSelectProps) => {
   return (
     <div className="flex w-[330px] h-[456px]">
       <div className="flex flex-col relative top-0 justify-between w-[330px] h-[263px]">
-        <Coin coin={coins[0]} />
-        <Coin coin={coins[1]} />
-        <Coin coin={coins[2]} />
-        <Coin coin={coins[3]} />
+        {Coins.map(props => <Coin {...props} />}
       </div>
       <p className="flex relative bottom-[167px] w-[330px] h-[1px] fill-gray-6" />
       <div className="flex flex-col relative bottom-[21px] justify-between w-[330px] h-[121px]">
-        <Stock stock={stocks[0]} />
-        <Stock stock={stocks[1]} />
+        {Stocks.map(props => <Stock {...props} />}
       </div>
     </div>
   );
